a93eb8c6d405004ba2ecb454d6256124420feb41
mab_gray <m2bbie@gmail.com>
2023-03-16 13:28:24 -0500

    Assign dish button

--- js/script.js
+++ js/script.js
@@ -8,8 +8,10 @@ const guestInput = document.querySelector(".add-guest input");
 const guestList = document.querySelector(".guest-list");
 // span class for number of guests attending
 const guestCount = document.querySelector(".attendance");
-// alert when guest list is full (not yet visible)
+// alert when guest list is full (not yet visible\)
 const guestFull = document.querySelector(".alert");
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assignedItems");
 
 addGuestButton.addEventListener("click", function(){
     let guest = guestInput.value;
@@ -47,3 +49,23 @@ const updateGuestCount = function(){
 
         }
 }
+
+const assignItems = function(){
+    let potLuckItems = ["tofu scramble", "bean and cheese quesidillas", "kale chips","blondies", "ooey gooey bars", "macaroni salad", "buffalo cauliflower", "watermelon", "limeade", "grapes", "potato chips","veggie burger sliders"];
+
+    const allGuests = document.querySelectorAll(".guestList li");
+        for(var guest of allGuests){
+            let randomPotLuckIndex = Math.floor(Math.random() * potLuckItems.length);
+            let randomPotLuckItem = potLuckItems[randomPotLuckIndex];
+            listItem.innerText = `hey ${guest.innerText} can you bring ${randomPotLuckItem}?`
+            assignedItems.append(listItem);
+            potLuckItems.splice(randomPotLuckIndex, 1);
+
+        }
+
+}
+
+assignButton.addEventListener("click", function(){
+    assignItems();
+    assignButton.disabled = true;
+});
